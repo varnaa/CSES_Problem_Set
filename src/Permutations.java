24bdc6e5381bc1193549564c4bb9558a636c8747
varnaa <swersie723>
2020-06-28 18:24:28 +0530

    Optimized Permutations solution.

--- src/Permutations.java
+++ src/Permutations.java
@@ -6,8 +6,13 @@ public class Permutations {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
     findPermutation(n);
+    findPermutation1(n);
+    findPermutation2(n);
   }
 
+  // Method 1
+  // This solution doesn't work for n = 4 but works for all cases n > 4.
+  // 0.49ms
   public static void findPermutation(int n) {
     int[] output = new int[n];
     if (n == 1) {
@@ -15,9 +20,6 @@ public class Permutations {
     }
     if (n == 2 || n == 3) {
       System.out.println("NO SOLUTION");
-    }
-    if (n == 4) {
-      System.out.println("2 4 1 3");
     } else {
       int evenIndex = 1, oddIndex = n - 2;
       output[0] = n % 2 == 0 ? n : n - 1;
@@ -31,13 +33,61 @@ public class Permutations {
         output[oddIndex] = output[oddIndex + 1] + 2;
         oddIndex--;
       }
+      StringBuilder stringBuilder = new StringBuilder();
+      for (int values : output) {
+        stringBuilder.append(values).append(" ");
+      }
+
+      System.out.println(stringBuilder);
     }
 
-    StringBuilder stringBuilder = new StringBuilder();
-    for (int values : output) {
-      stringBuilder.append(values).append(" ");
-    }
+  }
 
-    System.out.println(stringBuilder);
+  // Method 2: Works for all solution n > 3
+  // Instead of arranging from increasing to decreasing of odd and even numbers, arrange it from decreasing to increasing.
+  // TLE for large inputs.
+
+  public static void findPermutation1(int n) {
+
+    if (n == 1) {
+      System.out.println("1");
+    }
+    if (n <= 3) {
+      System.out.println("NO SOLUTION");
+    } else {
+      StringBuilder odd = new StringBuilder();
+      for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+          System.out.print(i + " ");
+        } else {
+          odd.append(i).append(" ");
+        }
+      }
+      System.out.print(odd);
+    }
+  }
+
+  // Method 3: Same as method 2 but instead of sout for even numbers using a string.
+  // Passes for large inputs : 0.44ms
+  public static void findPermutation2(int n) {
+
+    if (n == 1) {
+      System.out.println("1");
+    }
+    if (n <= 3) {
+      System.out.println("NO SOLUTION");
+    } else {
+      StringBuilder odd = new StringBuilder();
+      StringBuilder even = new StringBuilder();
+      for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+          even.append(i).append(" ");
+        } else {
+          odd.append(i).append(" ");
+        }
+      }
+      System.out.print(even);
+      System.out.print(odd);
+    }
   }
 }
