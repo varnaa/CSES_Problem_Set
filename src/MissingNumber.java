7788a4628f1e27c061d44881816dc3431bdcd33c
varnaa <swersie723>
2020-06-28 15:23:02 +0530

    Solved MissingNumber. closes #2.

---
+++ src/MissingNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        // Find the Missing Number.
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[n - 1];
+        long actualSum = (long) n * (n + 1) / 2;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = in.nextInt();
+            actualSum -= array[i];
+        }
+        System.out.println(actualSum);
+    }
+}
